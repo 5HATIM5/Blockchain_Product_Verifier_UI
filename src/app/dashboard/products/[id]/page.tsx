d762833f3ec00175324653178223c8cd3f84d08f
Hatim Shabbir <hatim.shabbir5152@gmail.com>
2024-02-05 11:49:42 +0100

    UI Updates: SingleProductPage

--- src/app/dashboard/products/[id]/page.tsx
+++ src/app/dashboard/products/[id]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Loader from "@/app/ui/Components/Global/Loader";
 import { GetProductsById, UpdateProduct } from "@/lib/data";
 import { useParams } from "next/navigation";
 import { useRouter } from "next/navigation";
@@ -48,7 +49,7 @@ const SingleProductPage = (props: Props) => {
   }, []);
 
   return !product ? (
-    <h1>Loading</h1>
+    <Loader/>
   ) : (
     <main className="flex min-h-screen flex-col justify-start items-center p-7">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] pb-10 z-[-1]">
